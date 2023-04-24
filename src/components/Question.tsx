27706c389a3a73a9264f992512972c2184b81b5e
Halyna Stepanenko <galinavikst@gmail.com>
2023-04-24 23:18:29 +0200

    Updates

--- src/components/Question.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { inputQuestionValue } from "../store/questionFormSlice";
-import { useSelector } from "react-redux";
-
-export function Question() {
-  const question = useSelector(inputQuestionValue);
-  return <h2>{question}</h2>;
-}
