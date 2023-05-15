ddc68670d0e608baf9ad3612e5deeb2bf911eff9
Halyna Stepanenko <galinavikst@gmail.com>
2023-05-15 15:42:27 +0200

    feat: implement lookup service

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { allClients } from "../data";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,6 +5,7 @@ import { setQuestion } from "../store/questionFormSlice";
 import { IOption, options, setOptions } from "../store/optionsFormSlice";
 import createVotingBlock from "../components/servise";
 import { isPercentage, isResult } from "../store/votingPageSlice";
+import { useState } from "react";
 
 interface IQuestion {
   question: string;
@@ -13,6 +13,7 @@ interface IQuestion {
 }
 
 export default function HomePage() {
+  const [seachInputValue, setSeachInputValue] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const optionsArr = useSelector(options);
@@ -32,20 +33,60 @@ export default function HomePage() {
     navigate("/voting-page");
   };
 
+  const handleOnChange = (inputValue: string) => {
+    setSeachInputValue(inputValue);
+  };
+
   const questions = allClients.flatMap((el) => {
     return el.questions.map((item) => {
-      return (
-        <li key={item.question} onClick={() => goVote(item)}>
-          {item.question}
-        </li>
-      );
+      if (
+        item.question.toLowerCase().indexOf(seachInputValue.toLowerCase()) ===
+        -1
+      )
+        return null; //if there is no matches beetween searchInput and questions
+
+      return isHighlightedQuestionItems(item, seachInputValue, goVote);
     });
   });
 
   return (
     <div>
       <button onClick={sendToQuestionForm}>create your own question</button>
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => handleOnChange(e.target.value)}
+          value={seachInputValue}
+        />
+      </form>
       <ol>{questions}</ol>
     </div>
+  );
+}
+
+//handle the correct execution of questions on the page based on the search input
+function isHighlightedQuestionItems(
+  item: IQuestion,
+  seachInputValue: string,
+  goVoteFn: (item: IQuestion) => void
+) {
+  const regex = new RegExp(`(${seachInputValue})`, "gi"); // global, case insensitive
+  const partsArr = item.question.split(regex);
+
+  return (
+    <li key={item.question} onClick={() => goVoteFn(item)}>
+      {seachInputValue
+        ? partsArr.map((part, index) =>
+            regex.test(part) ? (
+              <span key={index} className="highlighted">
+                {part}
+              </span>
+            ) : (
+              <span key={index}>{part}</span>
+            )
+          )
+        : item.question}
+    </li>
   );
 }
